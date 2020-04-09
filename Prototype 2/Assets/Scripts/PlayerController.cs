2c4c87d7a994e5107b62ce9b4683149c76f5309c
FrolicHard <58254450+FrolicHard@users.noreply.github.com>
2020-04-09 13:18:06 +0300

    Unit2: Feeding the animals
    A lil' top down Space Invaders-like  excercise. Learning prefabs, collision detection etc.

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    private float speed = 13.0f;
+    private float xRange = 21;
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //boundaries
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //Player movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
