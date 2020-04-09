eae47735806e055ee0c9567f574cd524ac39280e
FrolicHard <58254450+FrolicHard@users.noreply.github.com>
2020-04-09 14:19:05 +0300

    Challenge 2: Playing catch
    Completed the challenge including the bonus objectives

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnRate = 0.8f;
+    private float lastSpawn = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time > spawnRate + lastSpawn)
+            {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawn = Time.time;
+            }
+        }
+    }
+}
