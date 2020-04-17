20c3b1894b3c624723de441b440f7d3711d8e0aa
FrolicHard <58254450+FrolicHard@users.noreply.github.com>
2020-04-17 17:42:47 +0300

    Prototype 3 and Challenge 3
    Uploading Prototype 3 and Challenge 3, both completed, including bonus objectives.

---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator playerAnim;
+    private Rigidbody playerRb;
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 0.5f);
+            dirtParticle.Stop();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        } 
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(crashSound, 0.5f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
+
+            
+        }
+    }
+}
